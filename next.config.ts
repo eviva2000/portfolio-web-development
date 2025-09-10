c885c27df2737212750de222c8fdcc28732a8885
eviva2000 <arjomandielaheh@gmail.com>
2025-09-10 16:33:53 +0200

    fix image type

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+const nextConfig: {
+  images: {
+    remotePatterns: {
+      protocol: string;
+      hostname: string;
+      port: string;
+      pathname: string;
+    }[];
+    domains: string[];
+  };
+  experimental?: {
+    swcPlugins: [string, Record<string, unknown>][];
+  };
+} = {
   images: {
     remotePatterns: [
       {
@@ -17,13 +30,8 @@ const nextConfig = {
 
 if (process.env.NEXT_PUBLIC_TEMPO) {
   nextConfig["experimental"] = {
-      // NextJS 13.4.8 up to 14.1.3:
-      // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
-      // NextJS 14.1.3 to 14.2.11:
-      swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
-
-      // NextJS 15+ (Not yet supported, coming soon)
-  }
+    swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
+  } as { swcPlugins: [string, Record<string, unknown>][] }; // Explicit type assertion
 }
 
 module.exports = nextConfig;
